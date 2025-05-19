182b4b7e1acc60fd6c8a2385e091d02b57ee4f77
Aryan1711-rookie <workaryan@outlook.com>
2025-05-19 23:21:05 +0530

    Fix issue

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import env from 'dotenv';
-import connectDB from './utils/db.js';
-import userRoutes from './routes/user.route.js';
-import blogs from './routes/blog.route.js'
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import env from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoutes from "./routes/user.route.js";
+import blogs from "./routes/blog.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 env.config();
 const app = express();
@@ -20,17 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ["https://weeklydigest-blog.onrender.com"],
-    credentials : true,
-    optionSuccessStatus: 200
+  origin: "https://weeklydigest-blog.onrender.com",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 //routes
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/blog', blogs);
-
-
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogs);
 
 // app.use(express.static(path.join(__dirname, "/weeklyDigest/dist")));
 
@@ -43,7 +43,7 @@ app.use('/api/v1/blog', blogs);
 // });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {  
-    connectDB();
-    console.log(`Server is running on ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on ${PORT}`);
+});
